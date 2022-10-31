ada1b0a60759460a88ed800d2ae86293190a1c69
Darby Biddy <darbybiddy3@gmail.com>
2022-10-31 15:12:27 -0500

    added the post notes

---
+++ routes/api-routes.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const fs = require("fs")
+const uniqid = require('uniqid')
+
+router.get('/api/notes', (req,res)=>{
+    fs.readFile('db/db.json', 'utf8', (err,data)=>{
+        if(err)throw err
+        return res.json(JSON.parse(data))
+    })
+})
+
+router.post('/api/notes', (req,res)=>{
+    const dbData= JSON.parse(fs.readFileSync("db/db.json"));
+        const newNotes = req
+        const id = 'id'
+        const noteId = uniqid()
+        newNotes[id] = noteId
+        dbData.push(newNotes)
+        fs.writeFileSync('db/db.json', JSON.stringify(dbData), (err)=>{
+            if(err)throw err
+            res.json(dbData)
+        })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const htmlRoutes = require('./routes/html-routes')
+const apiRoutes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use(apiRoutes)
 app.use(htmlRoutes)
 
 app.listen(PORT, () =>
