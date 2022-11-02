8fae88eeca4f20598ecc1e97cab3f555fd41efd9
Darby Biddy <darbybiddy3@gmail.com>
2022-11-01 19:10:28 -0500

    fixed error and finished homework

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const fs = require("fs")
 const uniqid = require('uniqid')
+const db = require("../db/db.json")
 
 router.get('/api/notes', (req,res)=>{
     fs.readFile('db/db.json', 'utf8', (err,data)=>{
@@ -10,21 +11,20 @@ router.get('/api/notes', (req,res)=>{
     })
 })
 
-router.delete('/api/notes:id', (req,res)=>{
-    deleteNotes(req.params.id)
-    res.status(200).send()
-})
+// router.delete('/api/notes:id', (req,res)=>{
+//     deleteNotes(req.params.id)
+//     res.status(200).send()
+// })
 
 router.post('/api/notes', (req,res)=>{
-    const dbData= JSON.parse(fs.readFileSync("db/db.json"));
-        const newNotes = req
-        const id = 'id'
-        const noteId = uniqid()
-        newNotes[id] = noteId
-        dbData.push(newNotes)
-        fs.writeFileSync('db/db.json', JSON.stringify(dbData), (err)=>{
+        const newNotes = req.body
+        const id =  db.length + 1
+        newNotes.id = id
+        db.push(newNotes)
+        fs.writeFileSync('db/db.json', JSON.stringify(db), (err)=>{
             if(err)throw err
-            res.json(dbData)
+            res.json(db)
         })
 })
 
+module.exports = router
